a84b6732789ae5dbac26f5bdb2f1bfc19d145b2d
Otto <heinz-otto.klas@gmx.de>
2022-03-07 22:16:29 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -118,6 +118,19 @@ function StartFHEM {
 
 ### Start of Script ###
 
-StartFHEM
+if [ "$1" = 'init' ]; then
+    if [ ! -e /opt/fhem/fhem.pl ]; then
+      InitFHEM
+    fi
+fi
+if [ "$1" = 'start' ]; then
+    if [ "$2" = 'demo' ]; then
+       echo 'exec "perl fhem.pl fhem.cfg.demo"'
+    else
+      StartFHEM
+	fi
+else
+    exec "$@"
+fi
 
 #EOF
